72aca64ee1d3ded41b532281249db288298b9319
Maria Frolova <maria.frolova15@gmail.com>
2025-04-21 01:11:28 +0300

    Добавляет файл thumbnails.js и отрисовывает миниатюры на сайте

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
-import {photosArray} from './create-photos-array.js';
+import { photosArray } from './create-photos-array.js';
+import { renderThumbnails } from './thumbnails.js';
+
 // eslint-disable-next-line no-console
 console.log(photosArray);
+
+renderThumbnails(photosArray);
