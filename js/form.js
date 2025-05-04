e05e3a31e9ce66a87a76a731dc4491f3c0d455ca
Maria Frolova <maria.frolova15@gmail.com>
2025-05-04 18:04:37 +0300

    1. Добавляет модуль отвечающий за работу с формой и валидацию. 2. Вносит правки в задание из 8 модуля

---
+++ js/form.js
@@ -0,0 +1,64 @@
+import { validateForm } from './validation';
+
+const imageUploadInput = document.querySelector('.img-upload__input');
+const imageUploadModal = document.querySelector('.img-upload__overlay');
+const previewImageElement = imageUploadModal.querySelector('.img-upload__preview img');
+const effectPreviewIcons = imageUploadModal.querySelectorAll('.effects__preview');
+const cancelUploadButton = document.querySelector('.img-upload__cancel');
+const uploadFormElement = document.querySelector('.img-upload__form');
+
+const updateImagePreview = () => {
+  const fileImage = imageUploadInput.files[0];
+  previewImageElement.src = URL.createObjectURL(fileImage);
+  effectPreviewIcons.forEach((icon) => {
+    icon.style.backgroundImage = `url("${URL.createObjectURL(fileImage)}")`;
+  });
+};
+
+const openUploadModal = () => {
+  imageUploadModal.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  updateImagePreview();
+  document.addEventListener('keydown', onClickEsc);
+  imageUploadModal.addEventListener('click', onClickOutside);
+};
+
+const closeUploadModal = () => {
+  imageUploadModal.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  uploadFormElement.reset();
+  document.removeEventListener('keydown', onClickEsc);
+  imageUploadModal.removeEventListener('click', onClickOutside);
+};
+
+uploadFormElement.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  if (validateForm()) {
+    closeUploadModal();
+  }
+});
+
+imageUploadInput.addEventListener('change', () => {
+  openUploadModal();
+});
+
+cancelUploadButton.addEventListener('click', () => {
+  closeUploadModal();
+});
+
+function onClickEsc(evt) {
+  const isFocusedInput = evt.target.classList.contains('text__hashtags') || evt.target.classList.contains('text__description');
+  if (isFocusedInput) {
+    return false;
+  }
+  if (evt.key === 'Escape') {
+    closeUploadModal();
+  }
+}
+
+function onClickOutside(evt) {
+  if (evt.target.classList.contains('img-upload__overlay')) {
+    closeUploadModal();
+  }
+}
---
+++ js/validation.js
@@ -0,0 +1,70 @@
+import { isStringLengthValid } from './util.js';
+import {
+  MAX_DESCRIPTION_LENGTH,
+  MAX_HASHTAGS_COUNT,
+  HASHTAG_REGEX,
+  MAX_HASHTAGS_LENGTH
+} from './data.js';
+
+const uploadForm = document.querySelector('.img-upload__form');
+const hashtagsField = document.querySelector('.text__hashtags');
+const descriptionField = document.querySelector('.text__description');
+
+const pristine = new Pristine(
+  uploadForm,
+  {
+    classTo: 'img-upload__field-wrapper',
+    errorTextParent: 'img-upload__field-wrapper',
+    errorTextTag: 'p',
+    errorTextClass: 'form__error'
+  },
+);
+
+const isDescriptionValid = (value) => isStringLengthValid(value, MAX_DESCRIPTION_LENGTH);
+
+pristine.addValidator(
+  descriptionField,
+  isDescriptionValid,
+  `Длина описания не должна превышать ${MAX_DESCRIPTION_LENGTH} символов.`
+);
+
+const isHashtagsCountValid = (value) => value.replace(/ +/g, ' ').trim().split(' ').length <= MAX_HASHTAGS_COUNT;
+
+pristine.addValidator(
+  hashtagsField,
+  isHashtagsCountValid,
+  `Максимум ${MAX_HASHTAGS_COUNT} хештегов.`,
+
+  true
+);
+
+const areHashtagsValid = (value) => {
+  const tags = value.replace(/ +/g, ' ').trim().split(' ');
+  return !tags.some((tag) => !HASHTAG_REGEX.test(tag));
+};
+
+pristine.addValidator(
+  hashtagsField,
+  areHashtagsValid,
+  `Хештег должен начинаться с # и содержать только буквы и цифры; максимальная длина одного хэштега ${MAX_HASHTAGS_LENGTH} символов.`,
+
+  true
+);
+
+const areHashtagsUnique = (value) => {
+  const tags = value.replace(/ +/g, ' ').trim().toLowerCase().split(' ');
+  const uniqueTags = [...new Set(tags)];
+  return tags.length === uniqueTags.length;
+};
+
+pristine.addValidator(
+  hashtagsField,
+  areHashtagsUnique,
+  'Хештеги должны быть уникальными.',
+
+  true
+);
+
+const validateForm = () => pristine.validate();
+
+export { validateForm };
