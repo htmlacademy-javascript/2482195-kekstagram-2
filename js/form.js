0b3b636f724f8eabc79dcf2b80ae647a5e98824c
Maria Frolova <maria.frolova15@gmail.com>
2025-05-22 01:31:22 +0300

    Дорабатывает форму загрузки изображения и проверяет типы загружаемых файлов

--- js/form.js
+++ js/form.js
@@ -2,7 +2,7 @@ import { sendPhotos } from './api.js';
 import { validateForm, resetValidation } from './validation.js';
 import { resetScale } from './scale-editor.js';
 import { resetEffects } from './effect-editor.js';
-import { SUBMIT_BUTTON_TEXT, SUBMISSION_STATE, POPUP_TYPE } from './constants.js';
+import { SUBMIT_BUTTON_TEXT, SUBMISSION_STATE, POPUP_TYPE, FILE_TYPES, FILE_ERROR_MESSAGE } from './constants.js';
 import { showPopup } from './notifications.js';
 
 const imageUploadInput = document.querySelector('.img-upload__input');
@@ -25,25 +25,40 @@ const setSubmitButtonState = (state) => {
 
 const updateImagePreview = () => {
   const fileImage = imageUploadInput.files[0];
-  previewImageElement.src = URL.createObjectURL(fileImage);
-  effectPreviewIcons.forEach((icon) => {
-    icon.style.backgroundImage = `url("${URL.createObjectURL(fileImage)}")`;
-  });
+  const fileName = fileImage.name.toLowerCase();
+  const matches = FILE_TYPES.some((type) => fileName.endsWith(type));
+
+  if (matches) {
+    const imageURL = URL.createObjectURL(fileImage);
+    previewImageElement.src = imageURL;
+
+    effectPreviewIcons.forEach((icon) => {
+      icon.style.backgroundImage = `url('${imageURL}')`;
+    });
+
+    imageUploadModal.classList.remove('hidden');
+    document.body.classList.add('modal-open');
+  } else {
+    showPopup(POPUP_TYPE.ERROR, FILE_ERROR_MESSAGE);
+    uploadFormElement.reset();
+  }
 };
 
 const openUploadModal = () => {
+  updateImagePreview();
   imageUploadModal.classList.remove('hidden');
   document.body.classList.add('modal-open');
-  updateImagePreview();
 
   document.addEventListener('keydown', onEscapeForm);
   imageUploadModal.addEventListener('click', onClickOutside);
 
   resetEffects();
-
   resetScale();
+  resetValidation();
 
 };
+
+imageUploadInput.addEventListener('change', openUploadModal);
 
 const closeUploadModal = () => {
   imageUploadModal.classList.add('hidden');
@@ -77,10 +92,6 @@ uploadFormElement.addEventListener('submit', (evt) => {
         setSubmitButtonState(SUBMISSION_STATE.IDLE);
       });
   }
-});
-
-imageUploadInput.addEventListener('change', () => {
-  openUploadModal();
 });
 
 cancelUploadButton.addEventListener('click', () => {
